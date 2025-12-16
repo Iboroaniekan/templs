0e22591c5fe711137f75809bcbb2b7c6147879a6
Iboroaniekan <iboroaniekan40@gmail.com>
2025-12-17 00:22:07 +0100

    Final touch done

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ pwshowHideBtn = document.querySelectorAll('.pw-hide');
 
 formOpenBtn.addEventListener('click', () => home.classList.add('show'))
 formCloseBtn.addEventListener('click', () => home.classList.remove('show'));
+
+pwshowHideBtn.forEach(icon => {
+    icon.addEventListener('click', () => {
+    let getPwInput = icon.parentElement.querySelector('input');
+    if(getPwInput.type === 'password'){
+        getPwInput.type = 'text';
+        icon.classList.replace('fa-eye-slash', 'fa-eye');
+    } else {
+        getPwInput.type = 'password';
+        icon.classList.replace('fa-eye', 'fa-eye-slash');
+    }
+    });
+})
+signupBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+    formContainer.classList.add('active');
+});
+
+
+loginBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+    formContainer.classList.remove('active');
+});
